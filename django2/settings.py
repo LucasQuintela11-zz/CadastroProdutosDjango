6a2a92ddd9778d5e61b3a10a926a5212d3417b90
Lucas Quintela <lucasquintela.dev@gmail.com>
2022-01-10 14:57:48 -0300

    created form contact

--- django2/settings.py
+++ django2/settings.py
@@ -130,3 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Configurações de e-mail
+
+
